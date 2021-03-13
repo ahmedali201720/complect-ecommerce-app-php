a4f6b5f45d373504ba94faa66e3fdfa3a7d9341c
Ahmed Ali <ahmedalimohamed1994@gmail.com>
2021-03-13 23:10:36 +0200

    moduarizing project includes

--- public/index.php
+++ public/index.php
@@ -1,3 +1,8 @@
+<?php
+
+require_once("../resources/functions.php");
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
---
+++ resources/config.php
@@ -0,0 +1,21 @@
+<?php
+
+ob_start();
+
+session_start();
+
+// Directory Separator for OS.
+defined("DS") ? null : define("DS",DIRECTORY_SEPARATOR);
+
+// Defining Paths for front and back Templates.
+defined("FRONT_TEMPLATE") ? null : define("FRONT_TEMPLATE", __DIR__ . DS . "templates" . DS . "front");
+defined("BACK_TEMPLATE") ? null : define("BACK_TEMPLATE", __DIR__ . DS . "templates" . DS . "back");
+
+// Database Configuration
+defined("DB_HOST") ? null : define("DB_HOST", "localhost");
+defined("DB_USER") ? null : define("DB_USER", "root");
+defined("DB_PASSWORD") ? null : define("DB_PASSWORD", "");
+defined("DB_NAME") ? null : define("DB_NAME", "ecommerce_db");
+
+// Database Connection
+$connection = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD);
