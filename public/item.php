fd2d9ef12aac5a7e1484170e162332df2e73f45a
Ahmed Ali <ahmedalimohamed1994@gmail.com>
2021-04-02 00:54:58 +0200

    single product data binding

--- public/item.php
+++ public/item.php
@@ -15,9 +15,7 @@
     $query = "SELECT * FROM products WHERE id =" .escape_string($_GET['id']). " " ;
     $result = query($query);
     confirm($result);
-    while ($row = fetch_array($result)){
-        echo $row['price'];
-    }
+    while ($row = fetch_array($result)):
 
     ?>
 
@@ -28,7 +26,7 @@
 <div class="row">
 
     <div class="col-md-7">
-       <img class="img-responsive" src="http://placehold.it/700x600" alt="">
+       <img class="img-responsive" src=<?php echo $row['product_image'] ;?> alt="">
 
     </div>
 
@@ -38,9 +36,9 @@
          
 
     <div class="caption-full">
-        <h4><a href="#">Javascript Course</a> </h4>
+        <h4><a href="#"><?php echo $row['title'] ?></a> </h4>
         <hr>
-        <h4 class="">$24.99</h4>
+        <h4 class=""><?php echo $row['price'] . "&#36;" ;?></h4>
 
     <div class="ratings">
      
@@ -54,7 +52,7 @@
         </p>
     </div>
           
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+        <p><?php echo $row['short_desc'] ;?></p>
 
    
     <form action="">
@@ -94,13 +92,8 @@
     <div role="tabpanel" class="tab-pane active" id="home">
 
 <p></p>
-           
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
 
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-
-
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+        <?php echo $row['description'] ;?>
 
     </div>
     <div role="tabpanel" class="tab-pane" id="profile">
@@ -206,6 +199,9 @@
 
 
 </div>
+
+    <?php endwhile ?>
+
 
 </div>
     <!-- /.container -->
