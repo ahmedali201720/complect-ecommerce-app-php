cd5a99543cbff1077dcfbbd5436812f97dd9d40c
Ahmed Ali <ahmedalimohamed1994@gmail.com>
2021-04-02 00:26:43 +0200

    working on single product functions

--- resources/functions.php
+++ resources/functions.php
@@ -26,3 +26,51 @@ function escape_string($string){
 function fetch_array($array){
     return mysqli_fetch_array($array);
 }
+
+function get_products(){
+    $query = query("SELECT * from products");
+    confirm($query);
+    while( $row = fetch_array($query) ){
+        $product = <<< DELIMITER
+                            <div class="col-sm-4 col-lg-4 col-md-4">
+                        <div class="thumbnail">
+                            <a href="item.php?id={$row['id']}"><img src="{$row['product_image']}" alt="product"></a>
+                            <div class="caption">
+                                <h4 class="pull-right">{$row['price']}</h4>
+                                <h4><a href="item.php?id={$row['id']}">{$row['title']}</a>
+                                </h4>
+                                <p>{$row['description']} <a target="_blank" href="http://www.bootsnipp.com">Bootsnipp - http://bootsnipp.com</a>.</p>
+                            </div>
+                            <div class="ratings">
+                                <p class="pull-right">15 reviews</p>
+                                <p>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+DELIMITER;
+
+        echo $product;
+
+
+    }
+}
+
+function get_categories(){
+    $query = query("SELECT * from categories");
+    confirm($query);
+    while( $row = fetch_array($query) ){
+        $category = <<< DELIMITER
+         <a href='category.php?id={$row['id']}' class='list-group-item'>{$row['title']}</a>                   
+DELIMITER;
+
+        echo $category;
+
+
+    }
+}
--- resources/templates/front/side_nav.php
+++ resources/templates/front/side_nav.php
@@ -3,14 +3,7 @@
 
     <?php
 
-         $query = "SELECT * FROM categories";
-         $categories = query($query);
-
-         confirm($categories);
-         
-         while($row = fetch_array($categories)){
-             echo "<a href='category.html' class='list-group-item'>{$row['title']}</a>";
-         }
+         get_categories();
 
     ?>
 </div>
